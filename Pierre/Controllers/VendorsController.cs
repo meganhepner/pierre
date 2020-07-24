39f712e9f536d97aa6939f8ec99fdf6839c42451
Megan Hepner <megan.hepner@gmail.com>
2020-07-24 14:15:06 -0700

    create show route on controller and show cshtml page

--- Pierre/Controllers/VendorsController.cs
+++ Pierre/Controllers/VendorsController.cs
@@ -20,23 +20,23 @@ namespace Pierre.Controllers
       return View();
     }
 
-    [HttpPost("/")]
+    [HttpPost("/vendors")]
     public ActionResult Create(string vendorName, string vendorDescription)
     {
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
-      return View();
+      return RedirectToAction("Index");
     }
 
-    // [HttpGet("vendors/{id")]
-    // public ActionResult Show(int id)
-    // {
-    //   Dictionary<string, object> model = new Dicitonary<string, object>();
-    //   Vendor selectedVendor = Vendor.Find(id);
-    //   List<Order> vendorOrders = selectedVendor.Orders;
-    //   model.Add("vendor", selectedVendor);
-    //   model.Add("orders", vendorOrders);
-    //   return View(model);
-    // }
+    [HttpGet("vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
 
 
 
